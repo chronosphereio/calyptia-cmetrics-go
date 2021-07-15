a2df09fff63046939a9c1a5fe3164f44ee6d148e
Jorge Niedbalski <jnr@metaklass.org>
2021-07-15 12:48:38 -0400

    add counter types
    Signed-off-by: Jorge Niedbalski <jnr@metaklass.org>

--- cmetrics_test.go
+++ cmetrics_test.go
@@ -44,31 +44,76 @@ func (suite *TestLibSuite) TestMultiContextFromMsgPack() {
 	err = gauge.Add(ts, 100.0, nil)
 	suite.Nil(err)
 
+	context3, err := NewContext()
+	suite.Nil(err)
+	suite.NotNil(context3)
+
+	counter, err := context3.CounterCreate("kubernetes", "network", "loads", "Network load", []string{"hostname", "app"})
+	suite.Nil(err)
+	suite.NotNil(counter)
+
+	err = counter.Set(ts, 100.0, nil)
+	suite.Nil(err)
+
+	context4, err := NewContext()
+	suite.Nil(err)
+	suite.NotNil(context4)
+
+	counter, err = context4.CounterCreate("kubernetes", "network", "loads", "Network load", []string{"hostname", "app"})
+	suite.Nil(err)
+	suite.NotNil(counter)
+
+	err = counter.Set(ts, 100.0, nil)
+	suite.Nil(err)
+
+	var buffer []byte
+
 	buffer1, err := context1.EncodeMsgPack()
 	suite.Nil(err)
 	suite.NotNil(buffer1)
 
+	buffer = append(buffer, buffer1...)
+
 	buffer2, err := context2.EncodeMsgPack()
 	suite.Nil(err)
 	suite.NotNil(buffer2)
+	buffer = append(buffer, buffer2...)
 
-	buffer := append(buffer1, buffer2...)
+	buffer3, err := context3.EncodeMsgPack()
+	suite.Nil(err)
+	suite.NotNil(buffer3)
+	buffer = append(buffer, buffer3...)
+
+	buffer4, err := context4.EncodeMsgPack()
+	suite.Nil(err)
+	suite.NotNil(buffer4)
+	buffer = append(buffer, buffer4...)
 
 	contextSet, err := NewContextSetFromMsgPack(buffer)
 	suite.Nil(err)
 	suite.NotNil(contextSet)
-	suite.Equal(len(contextSet), 2)
+	suite.Equal(len(contextSet), 4)
 
-	buffer3, err := contextSet[0].EncodeMsgPack()
+	buffer5, err := contextSet[0].EncodeMsgPack()
 	suite.Nil(err)
-	suite.NotNil(buffer3)
+	suite.NotNil(buffer5)
 
-	buffer4, err := contextSet[1].EncodeMsgPack()
+	buffer6, err := contextSet[1].EncodeMsgPack()
 	suite.Nil(err)
-	suite.NotNil(buffer4)
+	suite.NotNil(buffer6)
 
-	suite.Equal(buffer3, buffer1)
-	suite.Equal(buffer4, buffer2)
+	buffer7, err := contextSet[2].EncodeMsgPack()
+	suite.Nil(err)
+	suite.NotNil(buffer5)
+
+	buffer8, err := contextSet[3].EncodeMsgPack()
+	suite.Nil(err)
+	suite.NotNil(buffer6)
+
+	suite.Equal(buffer5, buffer1)
+	suite.Equal(buffer6, buffer2)
+	suite.Equal(buffer7, buffer3)
+	suite.Equal(buffer8, buffer4)
 
 }
 
